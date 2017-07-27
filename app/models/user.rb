5f61530e1e829aea9d4717e155273ad2463b3ff5
Sanjay Nelson & Zackory Taylor <pair+sanjaynelson.zacktaylor@devbootcamp.com>
2017-07-27 16:43:50 -0500

    create user validations & bcrypt methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ActiveRecord::Base
   has_many :answers, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :votes, foreign_key: :voter_id
+
+  validates :username, :email, presence: true
+  validate :validate_password
+
+  include BCrypt
+
+  def authenticate(input_password)
+    self.password == input_password
+  end
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @raw_password = new_password
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def validate_password
+    if @raw_password.nil?
+      errors.add(:password, "is required")
+    elsif @raw_password.length < 6
+      errors.add(:password, "must be 6 characters or more")
+    end
+  end
+
 end
