6c89cc4a9e74b13abe8897313728829b13de92b0
Sanjay Nelson & Zackory Taylor <pair+sanjaynelson.zacktaylor@devbootcamp.com>
2017-07-27 19:58:40 -0500

    fix login, add clickable button erb to questions

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,7 +9,7 @@ end
 post '/sessions' do
   @user = User.find_by(username: params[:user][:username])
 
-  if @user && @user.authenticate(params[:user][:password])
+  if @user && @user.verify_password(params[:user][:password])
     session[:user_id] = @user.id
     redirect '/'
   else
