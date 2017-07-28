e3bca9fbc408eb9aeba54427b94f3254e2c233b3
paul <hammerschmidtpaul@gmail.com>
2017-07-28 16:57:03 -0500

    seeds file creates comments for questions and answers and creates 50 votes of random values for each question, answer, and comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,14 @@ require 'faker'
 
 users_wanted = 4
 questions_wanted = 10
-answers_wanted = 24
-comments_wanted = 24
+answers_wanted = 16
+comments_wanted = 30
 
 users_needed = users_wanted - User.count
+
+def add_votes(table_entry)
+  rand(50).times { table_entry.votes << Vote.new(voter: User.all.sample, value: (rand(0..1) * 2 - 1)) }
+end
 
 users_needed.times do
   user = {
@@ -29,6 +33,8 @@ questions_needed.times do
 
   question = Question.new(question)
   question.save
+
+  add_votes(question)
 end
 
 answers_needed = answers_wanted - Answer.count
@@ -42,9 +48,11 @@ answers_needed.times do
 
   answer = Answer.new(answer)
   answer.save
+
+  add_votes(answer)
 end
 
-comments_to_questions_needed = (comments_wanted / 2) - Comment.count
+comments_to_questions_needed = comments_wanted - (Comment.count / 2)
 
 comments_to_questions_needed.times do
   comment = {
@@ -55,9 +63,11 @@ comments_to_questions_needed.times do
 
   comment = Comment.new(comment)
   comment.save
+
+  add_votes(comment)
 end
 
-comments_to_answers_needed = (comments_wanted / 2) - Comment.count
+comments_to_answers_needed = comments_wanted - (Comment.count / 2)
 
 comments_to_answers_needed.times do
   comment = {
@@ -68,4 +78,6 @@ comments_to_answers_needed.times do
 
   comment = Comment.new(comment)
   comment.save
+
+  add_votes(comment)
 end
