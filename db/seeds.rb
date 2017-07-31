0702e54588830ca0575b74066907fc38a9cc36ef
paul <hammerschmidtpaul@gmail.com>
2017-07-31 00:41:08 -0500

    more styling, index page no longer has voting buttons, borders around each answer and its comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,7 +52,7 @@ answers_needed.times do
   add_votes(answer)
 end
 
-comments_to_questions_needed = comments_wanted - (Comment.count / 3)
+comments_to_questions_needed = (comments_wanted / 3) - (Comment.count / 3)
 
 comments_to_questions_needed.times do
   comment = {
@@ -67,7 +67,7 @@ comments_to_questions_needed.times do
   add_votes(comment)
 end
 
-comments_to_answers_needed = comments_wanted - (Comment.count / 1.5)
+comments_to_answers_needed = (comments_wanted / 1.5).to_i - (Comment.count / 1.5).to_i
 
 comments_to_answers_needed.times do
   comment = {
